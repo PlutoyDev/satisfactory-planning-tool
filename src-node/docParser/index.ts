c8a1168d4f0f8a674f97201f81808cfddb8fce4d
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2024-01-21 19:36:07 +0800

    cli option to not minify

--- src-node/docParser/index.ts
+++ src-node/docParser/index.ts
@@ -1,16 +1,23 @@
-import { readFile, writeFile } from "fs/promises";
-import { parseRecipe } from "./recipeParser.js";
-import { parseItem } from "./itemParser.js";
-import { parseProductionMachine } from "./buildableParser.js";
-import { parsePowerGenerator } from "./generatorParser.js";
+import { readFile, writeFile } from 'fs/promises';
+import { parseRecipe } from './recipeParser.js';
+import { parseItem } from './itemParser.js';
+import { parseProductionMachine } from './buildableParser.js';
+import { parsePowerGenerator } from './generatorParser.js';
 
-const filePath = "./res/docs.json";
-const json = await readFile(filePath).catch((err) => {
+const minifyJson = !process.argv.includes('--no-minify');
+if (!minifyJson) {
+  console.log('Not minifying JSON');
+}
+
+const filePath = './res/docs.json';
+const json = await readFile(filePath).catch(err => {
   console.error(err);
-  console.log("Failed to read docs.json, docs.json must be manually copied to the root directory of this project.");
+  console.log(
+    'Failed to read docs.json, docs.json must be manually copied to the root directory of this project.'
+  );
   process.exit(1);
 });
-const decoder = new TextDecoder("utf-16le");
+const decoder = new TextDecoder('utf-16le');
 const docs = JSON.parse(decoder.decode(json)) as unknown;
 
 if (!docs || !Array.isArray(docs)) {
@@ -36,7 +43,7 @@ const results = {
 const classesList: { nativeClass: string; classes: string[] }[] = [];
 
 for (const doc of docs) {
-  const nc = doc["NativeClass"];
+  const nc = doc['NativeClass'];
   if (!nc) {
     continue;
   }
@@ -45,41 +52,60 @@ for (const doc of docs) {
     continue;
   }
   const className = match[1];
-  if (["FGItemDescriptor", "FGItemDescriptorBiomass"].includes(className)) {
+  if (['FGItemDescriptor', 'FGItemDescriptorBiomass'].includes(className)) {
     results.items = Object.assign(results.items, parseItem(doc.Classes));
-  } else if (className === "FGResourceDescriptor") {
-    results.resources = Object.assign(results.resources, parseItem(doc.Classes));
-  } else if (className === "FGRecipe") {
+  } else if (className === 'FGResourceDescriptor') {
+    results.resources = Object.assign(
+      results.resources,
+      parseItem(doc.Classes)
+    );
+  } else if (className === 'FGRecipe') {
     results.recipes = parseRecipe(doc.Classes);
   } else if (
     [
-      "FGBuildableManufacturer",
-      "FGBuildableManufacturerVariablePower",
-      "FGBuildableResourceExtractor",
-      "FGBuildableWaterPump",
+      'FGBuildableManufacturer',
+      'FGBuildableManufacturerVariablePower',
+      'FGBuildableResourceExtractor',
+      'FGBuildableWaterPump',
     ].includes(className)
   ) {
-    results.productionMachines = Object.assign(results.productionMachines, parseProductionMachine(doc.Classes));
-  } else if (className === "FGBuildableGeneratorFuel" || className === "FGBuildableGeneratorNuclear") {
-    results.generators = Object.assign(results.generators, parsePowerGenerator(doc.Classes));
+    results.productionMachines = Object.assign(
+      results.productionMachines,
+      parseProductionMachine(doc.Classes)
+    );
+  } else if (
+    className === 'FGBuildableGeneratorFuel' ||
+    className === 'FGBuildableGeneratorNuclear'
+  ) {
+    results.generators = Object.assign(
+      results.generators,
+      parsePowerGenerator(doc.Classes)
+    );
   }
 
   classesList.push({
     nativeClass: className,
-    classes: doc.Classes.map(({ ClassName }: Record<string, unknown>) => ClassName),
+    classes: doc.Classes.map(
+      ({ ClassName }: Record<string, unknown>) => ClassName
+    ),
   });
 }
 
 // PostProcess
 // Add recipeKeys to items and resources
-for (const item of [...Object.values(results.items), ...Object.values(results.resources)]) {
+for (const item of [
+  ...Object.values(results.items),
+  ...Object.values(results.resources),
+]) {
   const productOf: string[] = [];
   const ingredientOf: string[] = [];
-  for (const [recipeKey, { ingredients, products }] of Object.entries(results.recipes)) {
-    if (products?.some((product) => product.itemKey === item.key)) {
+  for (const [recipeKey, { ingredients, products }] of Object.entries(
+    results.recipes
+  )) {
+    if (products?.some(product => product.itemKey === item.key)) {
       productOf.push(recipeKey);
     }
-    if (ingredients?.some((ingredient) => ingredient.itemKey === item.key)) {
+    if (ingredients?.some(ingredient => ingredient.itemKey === item.key)) {
       ingredientOf.push(recipeKey);
     }
   }
@@ -93,4 +119,7 @@ for (const item of [...Object.values(results.items), ...Object.values(results.re
   }
 }
 
-await writeFile("./public/parsed-data.json", JSON.stringify(results, null, 2));
+await writeFile(
+  './public/parsed-data.json',
+  JSON.stringify(results, null, minifyJson ? undefined : 2)
+);
