8e59ef051978f489bb98dbec2305c80fee14de0e
PlutoyDev <plutoy.dev@outlook.com>
2024-01-25 07:51:57 +0800

    default title component

--- src/components/HeadDefault.tsx
+++ src/components/HeadDefault.tsx
@@ -2,6 +2,7 @@ export default function HeadDefault() {
   return (
     <>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <title key='title'>Satisfactory Production Planner</title>
       <meta name='description' content='Demo showcasing Vike' />
     </>
   );
