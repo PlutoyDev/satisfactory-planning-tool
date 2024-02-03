3b0795ff23c97ae1e792d627273c3f66f9ad1843
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2024-02-03 23:12:01 +0800

    reactflow styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppShell from './AppShell';
 import './index.css';
+import 'reactflow/dist/style.css';
 
 // biome-ignore lint/style/noNonNullAssertion: It's defined in index.html
 ReactDOM.createRoot(document.getElementById('root')!).render(
