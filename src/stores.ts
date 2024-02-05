1ee7973c4a13ebba56cd7f7277e12a3012e75926
PlutoyDev <plutoy.dev@outlook.com>
2024-02-05 20:53:20 +0800

    combining store into a file

---
+++ src/stores.ts
@@ -0,0 +1,163 @@
+// Main application state store using zustand
+import { create } from "zustand";
+import { nanoid } from "nanoid";
+import { addEdge, applyEdgeChanges, applyNodeChanges } from "reactflow";
+import type { Node, Edge, OnConnect, OnNodesChange, OnEdgesChange, NodeChange, EdgeChange, Connection } from "reactflow";
+import { get as idbGet, set as idbSet } from "idb-keyval";
+import { unzlibSync, zlibSync } from "fflate";
+import { produce } from "immer";
+
+interface ProdInfo {
+  id: string;
+  title: string;
+  icon: string;
+}
+
+interface Actions {
+  updateInfo: (id: string, info: Partial<Omit<ProdInfo, "id">>) => void;
+
+  createProdLine: () => void;
+  loadProdLine: (id: string) => void;
+  saveProdLine: () => void;
+  deleteProdLine: (id: string) => void;
+
+  // Callbacks for graph changes
+  onConnect: OnConnect;
+  onNodesChange: OnNodesChange;
+  onEdgesChange: OnEdgesChange;
+}
+
+interface GlobalState extends Actions {
+  // Will be in global state when in any route
+  state: string;
+  prodInfos: ProdInfo[];
+}
+
+interface ProdLine {
+  // Production line graph's nodes and edges
+  nodes: Node[];
+  edges: Edge[];
+}
+
+interface LoadingState extends GlobalState {
+  // Will be in loading state when in production-line route
+  state: "loading" | "loading-error";
+}
+
+interface EditingState extends GlobalState, ProdInfo, ProdLine {
+  // Will be in editing state when in production-line route
+  state: "editing";
+  // Currently editing Production line
+  prodId: string;
+  title: string;
+  icon: string;
+
+  isSaving: boolean;
+  isSaved: boolean;
+}
+
+interface HomeState extends GlobalState {
+  // Will be in home state when in home route
+  state: "home";
+}
+
+type State = EditingState | LoadingState | HomeState;
+
+function getStoredProdInfos(): ProdInfo[] {
+  const prods = localStorage.getItem("prods");
+  if (!prods || !Array.isArray(prods)) {
+    return [];
+  }
+  return prods;
+}
+
+function saveProdInfos(prods: ProdInfo[]) {
+  localStorage.setItem("prods", JSON.stringify(prods));
+}
+
+export const useStore = create<State>((set, get) => ({
+  state: "home",
+  prodInfos: getStoredProdInfos(),
+  updateInfo: (id, info) => {
+    const index = get().prodInfos.findIndex((prod) => prod.id === id);
+    if (index === -1) return;
+    set(
+      produce((state) => {
+        state.prodInfos[index] = { ...state.prodInfos[index], ...info };
+        saveProdInfos(state.prodInfos);
+      }),
+    );
+  },
+  createProdLine: () => {
+    const newProdInfo: ProdInfo = {
+      id: nanoid(),
+      title: "New Production Line",
+      icon: "???",
+    };
+    set({
+      state: "editing",
+      prodId: newProdInfo.id,
+      title: newProdInfo.title,
+      icon: newProdInfo.icon,
+      nodes: [],
+      edges: [],
+      isSaving: false,
+      isSaved: false,
+      prodInfos: [...get().prodInfos, newProdInfo],
+    });
+  },
+  loadProdLine: async (id: string) => {
+    // Load from indexeddb, its stored with prefix 'prod-'
+    set({ state: "loading" });
+    try {
+      const prodInfo = get().prodInfos.find((prod) => prod.id === id);
+      const prod = await idbGet(`prod-${id}`);
+      if (!prod || !prodInfo) {
+        return;
+      }
+      // Unzlib the data, and parse
+      const data = JSON.parse(atob(new TextDecoder().decode(unzlibSync(prod)))) as ProdLine;
+      set({
+        state: "editing",
+        ...prodInfo,
+        ...data,
+        isSaving: false,
+        isSaved: true,
+      });
+    } catch (error) {
+      console.error("Error loading production line", error);
+      set({ state: "loading-error" });
+    }
+  },
+  saveProdLine: async () => {
+    const store = get();
+    if (store.state !== "editing") return;
+    set({ isSaving: true });
+    const b64 = btoa(JSON.stringify({ nodes: store.nodes, edges: store.edges }));
+    const size = b64.length;
+    const compressed = zlibSync(new TextEncoder().encode(b64));
+    await idbSet(`prod-${store.prodId}`, compressed);
+  },
+  deleteProdLine: async (id: string) => {},
+  onNodesChange: (changes: NodeChange[]) => {
+    const store = get();
+    if (store.state !== "editing") return;
+    set({
+      nodes: applyNodeChanges(changes, store.nodes),
+    });
+  },
+  onEdgesChange: (changes: EdgeChange[]) => {
+    const store = get();
+    if (store.state !== "editing") return;
+    set({
+      edges: applyEdgeChanges(changes, store.edges),
+    });
+  },
+  onConnect: (connection: Connection) => {
+    const store = get();
+    if (store.state !== "editing") return;
+    set({
+      edges: addEdge(connection, store.edges),
+    });
+  },
+}));
