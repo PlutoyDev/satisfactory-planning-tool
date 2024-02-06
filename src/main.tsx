307d0688bed730844310600f981668feb5f8569d
PlutoyDev <plutoy.dev@outlook.com>
2024-02-06 13:44:11 +0800

    remove strict mode as its causing useEffect to run twice

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,4 @@ import './index.css';
 import 'reactflow/dist/style.css';
 
 // biome-ignore lint/style/noNonNullAssertion: It's defined in index.html
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AppShell />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<AppShell />);
