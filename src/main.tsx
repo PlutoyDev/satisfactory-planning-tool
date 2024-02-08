24c6463e2e118b5d6e9c8fbc0478c27f5bc125aa
PlutoyDev <plutoy.dev@outlook.com>
2024-02-08 12:50:34 +0800

    create useLegacyEffect to run effects once in strict mode
    re-enable strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import AppShell from './AppShell';
 import './index.css';
 import 'reactflow/dist/style.css';
 
 // biome-ignore lint/style/noNonNullAssertion: It's defined in index.html
-ReactDOM.createRoot(document.getElementById('root')!).render(<AppShell />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <AppShell />
+  </StrictMode>
+);
