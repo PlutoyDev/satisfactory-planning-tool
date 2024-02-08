dd775eef0c05de70c900a8654d6d2234a57b0b71
PlutoyDev <plutoy.dev@outlook.com>
2024-02-08 15:33:45 +0800

    top center toolbar shorten productionLineId to prodLineId

--- src/pages/ProductionGraph.tsx
+++ src/pages/ProductionGraph.tsx
@@ -2,6 +2,7 @@ import { useRef } from 'react';
 import { useRoute, useLocation } from 'wouter';
 import { useState, useCallback } from 'react';
 import ReactFlow, {
+  Panel,
   Background,
   addEdge,
   applyEdgeChanges,
@@ -14,31 +15,35 @@ import ReactFlow, {
 } from 'reactflow';
 import * as idb from 'idb';
 import useLegacyEffect from '../hooks/useLegacyEffect';
+import {
+  ArrowsPointingOutIcon,
+  ArrowsPointingInIcon,
+} from '@heroicons/react/24/outline';
 
 export const routePattern = '/production-lines/:id' as const;
 
 // Indexdb for storing production line data
 type DbNode = Pick<Node, 'id' | 'type' | 'data' | 'position'> & {
-  productionLineId: string;
+  prodLineId: string;
 };
 
 type DbEdge = Pick<
   Edge,
   'id' | 'type' | 'data' | 'source' | 'target' | 'sourceHandle' | 'targetHandle'
 > & {
-  productionLineId: string;
+  prodLineId: string;
 };
 
 interface IProductionLineDb extends idb.DBSchema {
   nodes: {
     key: string;
     value: DbNode;
-    indexes: { productionLineId: string };
+    indexes: { prodLineId: string };
   };
   edges: {
     key: string;
     value: DbEdge;
-    indexes: { productionLineId: string };
+    indexes: { prodLineId: string };
   };
 }
 
@@ -50,12 +55,12 @@ function openProdLineDb() {
   return idb.openDB<IProductionLineDb>('prodLines', 1, {
     upgrade(db) {
       db.createObjectStore('nodes', { keyPath: 'id' }).createIndex(
-        'productionLineId',
-        'productionLineId'
+        'prodLineId',
+        'prodLineId'
       );
       db.createObjectStore('edges', { keyPath: 'id' }).createIndex(
-        'productionLineId',
-        'productionLineId'
+        'prodLineId',
+        'prodLineId'
       );
     },
   });
@@ -66,6 +71,7 @@ export function ProductionGraph() {
   const [loading, setLoading] = useState(true);
   const [nodes, setNodes] = useState<Node[]>([]);
   const [edges, setEdges] = useState<Edge[]>([]);
+  const [isFullscreen, setIsFullscreen] = useState(false);
 
   const [, params] = useRoute(routePattern);
   const [, navigate] = useLocation();
@@ -88,8 +94,8 @@ export function ProductionGraph() {
   const loadProductionLine = useCallback(async (prodLineId: string) => {
     const db = await openProdLineDb();
     const [nodes, edges] = await Promise.all([
-      db.getAllFromIndex('nodes', 'productionLineId', prodLineId),
-      db.getAllFromIndex('edges', 'productionLineId', prodLineId),
+      db.getAllFromIndex('nodes', 'prodLineId', prodLineId),
+      db.getAllFromIndex('edges', 'prodLineId', prodLineId),
     ]);
     setNodes(nodes);
     setEdges(edges);
@@ -108,7 +114,7 @@ export function ProductionGraph() {
             type,
             data,
             position,
-            productionLineId: prodLineId,
+            prodLineId,
           })
         ),
         ...edges.map(
@@ -121,7 +127,7 @@ export function ProductionGraph() {
               target,
               sourceHandle,
               targetHandle,
-              productionLineId: prodLineId,
+              prodLineId,
             })
         ),
         nodeTx.done,
@@ -149,6 +155,20 @@ export function ProductionGraph() {
       console.log('Loading production line', params.id);
       loadProductionLine(params.id).then(() => setLoading(false));
     }
+
+    // Fullscreen change
+    const onFullscreenChange = (e: Event) => {
+      if (!document.fullscreenElement) {
+        // If user exit fullscreen using the escape key or the browser's UI
+        setIsFullscreen(false);
+      }
+    };
+
+    document.addEventListener('fullscreenchange', onFullscreenChange);
+
+    return () => {
+      document.removeEventListener('fullscreenchange', onFullscreenChange);
+    };
   }, [params?.id, loadProductionLine]);
 
   if (loading) {
@@ -156,7 +176,7 @@ export function ProductionGraph() {
   }
 
   return (
-    <div className='w-full h-full' ref={elRef}>
+    <div className='w-full h-full bg-base-300' ref={elRef}>
       <ReactFlow
         nodes={nodes}
         edges={edges}
@@ -167,6 +187,47 @@ export function ProductionGraph() {
         // TODO: Make a better attribution, then hide this (It doesn't look good with this background)
         proOptions={{ hideAttribution: false }}
       >
+        <Panel position='top-center'>
+          <div className='flex items-center space-x-2 bg-base-100 p-1 shadow-lg rounded-sm'>
+            <div className='tooltip tooltip-bottom' data-tip='Save'>
+              <button
+                className='btn btn-sm btn-square btn-ghost'
+                type='button'
+                onClick={() => {
+                  if (params?.id) {
+                    saveProductionLine(params.id, nodes, edges);
+                  } else {
+                    console.error('No production line id');
+                  }
+                }}
+              >
+                💾
+              </button>
+            </div>
+            <div className='tooltip tooltip-bottom' data-tip='Fullscreen'>
+              <button
+                className='btn btn-sm btn-square btn-ghost'
+                type='button'
+                onClick={() => {
+                  if (elRef.current) {
+                    if (isFullscreen) {
+                      document.exitFullscreen();
+                    } else {
+                      elRef.current.requestFullscreen();
+                    }
+                    setIsFullscreen(f => !f);
+                  }
+                }}
+              >
+                {isFullscreen ? (
+                  <ArrowsPointingInIcon className='w-5 h-5' />
+                ) : (
+                  <ArrowsPointingOutIcon className='w-5 h-5' />
+                )}
+              </button>
+            </div>
+          </div>
+        </Panel>
         <Background />
       </ReactFlow>
     </div>
