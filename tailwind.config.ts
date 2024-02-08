0b43d5ab30201021f70c5fb64d30c1bca2a17192
PlutoyDev <plutoy.dev@outlook.com>
2024-02-08 15:51:10 +0800

    custom theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,4 +7,22 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+  daisyui: {
+    logs: false,
+    themes: [
+      {
+        dark: {
+          primary: "#fa9549",
+          secondary: "#7fd5ff",
+          accent: "#8fff66",
+          neutral: "#4a4a55", // or 9698a7
+          "base-100": "#2d2d40",
+          info: "#498cfa",
+          success: "#5ffa49",
+          warning: "#fae449",
+          error: "#fa4949",
+        },
+      },
+    ],
+  },
 } satisfies Config;
