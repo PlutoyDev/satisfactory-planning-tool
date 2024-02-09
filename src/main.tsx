1c61779e28ec47a1efb2d96303372d962b6881f6
PlutoyDev <plutoy.dev@outlook.com>
2024-02-10 07:44:00 +0800

    add react draggable for drag and drop interactivity import index css after reactflow css, for overiding style in reactflow

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import AppShell from './AppShell';
-import './index.css';
 import 'reactflow/dist/style.css';
+import './index.css';
 
 // biome-ignore lint/style/noNonNullAssertion: It's defined in index.html
 ReactDOM.createRoot(document.getElementById('root')!).render(
