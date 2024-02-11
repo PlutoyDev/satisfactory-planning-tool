b46bb0866cfba1e9b5caa06d3b2ee3b5c9b118bf
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2024-02-11 21:49:49 +0800

    fix nodes are not deleted when recovering from save

--- src/lib/ProductionLine.ts
+++ src/lib/ProductionLine.ts
@@ -44,15 +44,22 @@ function openProdLineDb() {
   });
 }
 
-async function loadFromDb(id: string) {
-  const db = await openProdLineDb();
+async function loadFromDb(id: string, db?: Awaited<ReturnType<typeof openProdLineDb>>) {
+  let shouldClose = false;
+  if (!db) {
+    shouldClose = true;
+    db = await openProdLineDb();
+  }
+
   const [nodes, edges, viewport] = await Promise.all([
     db.getAllFromIndex('nodes', 'prodLineId', id),
     db.getAllFromIndex('edges', 'prodLineId', id),
     db.get('viewport', id),
   ]);
 
-  db.close();
+  if (shouldClose) {
+    db.close();
+  }
   return { nodes, edges, viewport };
 }
 
@@ -75,9 +82,16 @@ async function saveToDb(param: SaveProductionLineArgs) {
   const prodLineId = param.prodLineId ?? (console.warn('No id provided for saving'), nanoid());
   const { nodes, edges } = param;
   const db = await openProdLineDb();
+  const { nodes: eNodes, edges: eEdges } = await loadFromDb(prodLineId, db);
+
+  const delNodesIds = eNodes.reduce((acc, { id }) => (nodes.find(n => n.id === id) ? acc : [...acc, id]), [] as string[]);
+  const delEdgesIds = eEdges.reduce((acc, { id }) => (edges.find(e => e.id === id) ? acc : [...acc, id]), [] as string[]);
+
   const nodeTx = db.transaction('nodes', 'readwrite');
   const edgeTx = db.transaction('edges', 'readwrite');
   await Promise.all([
+    ...delNodesIds.map(id => nodeTx.store.delete(id)),
+    ...delEdgesIds.map(id => edgeTx.store.delete(id)),
     ...nodes.map(({ id, type, data, position }) => nodeTx.store.put({ id, type, data, position, prodLineId })),
     ...edges.map(({ id, type, data, source, target, sourceHandle, targetHandle }) =>
       edgeTx.store.put({
