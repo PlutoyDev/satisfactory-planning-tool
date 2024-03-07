62200c770f68d91e128d7979b73272af5dc8c6db
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2024-03-07 19:56:19 +0800

    computation for recipe node

--- src/lib/factoryCompute.ts
+++ src/lib/factoryCompute.ts
@@ -1,13 +1,16 @@
-import type { Docs } from '../context/DocsContext';
+import type { Docs, Item, Recipe } from '../context/DocsContext';
 import type { ItemNodeData, RecipeNodeData, LogisticNodeData } from '../components/FactoryGraph';
+import StoredClockspeed from '../utils/clockspeed';
 
 const FactoryIODirOrder = ['left', 'top', 'right', 'bottom'] as const;
 type FactoryIODir = (typeof FactoryIODirOrder)[number];
-type FactoryIO = `${FactoryIODir}:${'solid' | 'fluid'}:${'in' | 'out'}`;
+type NoDirFactoryIO = `${'solid' | 'fluid'}:${'in' | 'out'}`;
+type FactoryIO = `${FactoryIODir}:${NoDirFactoryIO}`;
 type FactoryIndexedIO = `${FactoryIO}:${0 | 1 | 2 | 3}`;
 
 interface ItemSpeed {
   itemId: string;
+  /** number of item per minute */
   speed: number;
 }
 
@@ -15,8 +18,6 @@ interface ComputeResult {
   factoryIO: FactoryIndexedIO[];
   /** the speed of each item in each factoryIO */
   itemSpeed?: Partial<Record<FactoryIndexedIO, ItemSpeed[]>>;
-  /** valid */
-  valid?: Partial<Record<FactoryIndexedIO, boolean>>;
 }
 
 interface ComputeArgs<D> {
@@ -26,7 +27,7 @@ interface ComputeArgs<D> {
   connectedResult: Partial<Record<FactoryIndexedIO, ItemSpeed[]>>;
 }
 
-export function computeItemNode({ data, d, connectedResult }: ComputeArgs<ItemNodeData>): null | ComputeResult {
+export function computeItemNode({ data, d }: ComputeArgs<ItemNodeData>): null | ComputeResult {
   const { itemId, speed = 0, io = 'both' } = data;
 
   if (!itemId) return null;
@@ -42,15 +43,57 @@ export function computeItemNode({ data, d, connectedResult }: ComputeArgs<ItemNo
     const id: FactoryIndexedIO = `left:${form}:in:0`;
     factoryIO.push(id);
     itemSpeed[id] = [{ itemId, speed }];
-    valid[id] = connectedResult[id] ? connectedResult[id]!.some(x => x.itemId === itemId && x.speed === speed) : false;
   }
 
   if (io === 'both' || io === 'out') {
     const id: FactoryIndexedIO = `right:${form}:out:0`;
     factoryIO.push(id);
     itemSpeed[id] = [{ itemId, speed }];
-    valid[id] = connectedResult[id] ? connectedResult[id]!.some(x => x.itemId === itemId && x.speed === speed) : false;
   }
 
   return { factoryIO, itemSpeed };
+}
+
+interface RecipeComputeResult extends ComputeResult {
+  recipe: Recipe;
+  items: Item[];
+}
+
+export function computeRecipeNode({ data, d, connectedResult }: ComputeArgs<RecipeNodeData>): null | RecipeComputeResult {
+  const { recipeId, storedCs = StoredClockspeed.FromDecimal(1) } = data;
+
+  if (!recipeId) return null;
+  const recipe = d.recipes[recipeId];
+  if (!recipe) return null;
+
+  const factoryIO: FactoryIndexedIO[] = [];
+  const itemSpeed: Partial<Record<FactoryIndexedIO, ItemSpeed[]>> = {};
+
+  const { ingredients, products, producedIn, manufactoringDuration } = recipe;
+  const items: Item[] = [];
+
+  // The higher the clockspeed, the faster the factory runs, shortening the time it takes to produce items
+  const clockspeed = StoredClockspeed.ToDecimal(storedCs);
+  const duration = manufactoringDuration / clockspeed / 60; // minutes
+
+  const totalLength = ingredients.length + products.length;
+  const ioCount: Record<NoDirFactoryIO, 0 | 1 | 2 | 3> = { 'solid:in': 0, 'solid:out': 0, 'fluid:in': 0, 'fluid:out': 0 };
+
+  for (let i = 0; i < totalLength; i++) {
+    const { itemKey, amount } = i < ingredients.length ? ingredients[i] : products[i - ingredients.length];
+    const item = d.items[itemKey];
+    if (!item) return null;
+    items.push(item);
+
+    const form: 'solid' | 'fluid' = item.form === 'liquid' || item.form === 'gas' ? 'fluid' : 'solid';
+    const io = i < ingredients.length ? 'in' : 'out';
+    const noDirId: NoDirFactoryIO = `${form}:${io}`;
+    const index = ioCount[noDirId]++ as 0 | 1 | 2 | 3;
+    const id: FactoryIndexedIO = `${i < ingredients.length ? 'left' : 'right'}:${noDirId}:${index}`;
+    factoryIO.push(id);
+    // the shorter the duration, the faster the factory runs, requiring more items to be produced, and producing more items.
+    itemSpeed[id] = [{ itemId: itemKey, speed: ((i < ingredients.length ? 1 : -1) * amount) / duration }];
+  }
+
+  return { factoryIO, itemSpeed, recipe, items };
 }
